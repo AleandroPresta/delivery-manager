4a175c22faf0d0193258053777b80cbc754c8915
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-07-27 16:02:13 +0200

    Made customers-list input for the customers-list component. Created buttons for adding and deleting a customer

--- frontend/delivery-manager-frontend/src/app/app.component.ts
+++ frontend/delivery-manager-frontend/src/app/app.component.ts
@@ -3,14 +3,32 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { CustomersListComponent } from "./customers-list/customers-list.component";
+import { Customer } from './customers-list/Customer';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CustomersListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, CustomersListComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'delivery-manager-frontend';
+    title = 'delivery-manager-frontend';
+
+    customersList: Customer[] = [
+        new Customer('John', 'Doe', 30, 'john.doe@gmail.com', '123 Main St'),
+        new Customer('Jane', 'Smith', 25, 'jane.smith@gmail.com', '456 Elm St'),
+        new Customer('Michael', 'Johnson', 40, 'michael.johnson@gmail.com', '789 Oak St'),
+        new Customer('Emily', 'Davis', 35, 'emily.davis@gmail.com', '321 Pine St'),
+        new Customer('David', 'Wilson', 28, 'david.wilson@gmail.com', '654 Birch St')
+    ];
+
+    editCustomer(customer: Customer) {
+        console.log('Edit customer', customer);
+    }
+
+    deleteCustomer(customer: Customer) {
+        console.log('Delete customer', customer);
+    }
 }
--- frontend/delivery-manager-frontend/src/app/customers-list/customers-list.component.ts
+++ frontend/delivery-manager-frontend/src/app/customers-list/customers-list.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Customer } from './Customer';
 import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'customers-list',
-  standalone: true,
-  imports: [NgFor],
-  templateUrl: './customers-list.component.html',
-  styleUrl: './customers-list.component.css'
+    selector: 'customers-list',
+    standalone: true,
+    imports: [NgFor],
+    templateUrl: './customers-list.component.html',
+    styleUrl: './customers-list.component.css'
 })
 export class CustomersListComponent {
 
-  customers: Customer[] = [
-    new Customer('John', 'Doe', 30, 'john.doe@gmail.com', '123 Main St'),
-    new Customer('Jane', 'Smith', 25, 'jane.smith@gmail.com', '456 Elm St'),
-    new Customer('Michael', 'Johnson', 40, 'michael.johnson@gmail.com', '789 Oak St'),
-    new Customer('Emily', 'Davis', 35, 'emily.davis@gmail.com', '321 Pine St'),
-    new Customer('David', 'Wilson', 28, 'david.wilson@gmail.com', '654 Birch St')
-  ];
+    @Input() customers: Customer[] = [];
+    @Output() editCustomerEmitter = new EventEmitter<Customer>();
+    @Output() deleteCustomerEmitter = new EventEmitter<Customer>();
+
+    editCustomer(customer: Customer) {
+        this.editCustomerEmitter.emit(customer);
+    }
+
+    deleteCustomer(customer: Customer) {
+        this.deleteCustomerEmitter.emit(customer);
+    }
 
 }
